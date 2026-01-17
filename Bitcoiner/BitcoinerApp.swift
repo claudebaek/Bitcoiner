9f87b736e71cd1b77e318debd3322096c8af327d
claudebaek <appleofeyes0321@gmail.com>
2026-01-17 18:39:41 +0900

    Initial Commit

---
+++ Bitcoiner/BitcoinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinerApp.swift
+//  Bitcoiner
+//
+//  Created by leejay100 on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitcoinerTests/BitcoinerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinerTests.swift
+//  BitcoinerTests
+//
+//  Created by leejay100 on 1/17/26.
+//
+
+import Testing
+@testable import Bitcoiner
+
+struct BitcoinerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
